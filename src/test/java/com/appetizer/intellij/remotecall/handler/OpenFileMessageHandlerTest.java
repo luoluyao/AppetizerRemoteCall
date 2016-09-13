6f9d6fc079b8366310425cda3cf4460966c40455
luoluyao <luoly@emnets.org>
2016-09-13 16:00:01 +0800

    first commit

---
+++ src/test/java/com/appetizer/intellij/remotecall/handler/OpenFileMessageHandlerTest.java
@@ -0,0 +1,113 @@
+package com.appetizer.intellij.remotecall.handler;
+
+import com.appetizer.intellij.remotecall.utils.FileNavigator;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class OpenFileMessageHandlerTest {
+
+  private static OpenFileMessageHandler handler;
+  private static StubFileNavigator fileNavigator;
+
+  @BeforeClass
+  public static void setUp() {
+    fileNavigator = new StubFileNavigator();
+    handler = new OpenFileMessageHandler(fileNavigator);
+  }
+
+  @Test
+  public void handlerShouldExtractFilenameAndLineFromMessage() {
+    handler.handleMessage("FileName.java:80");
+    assertEquals("FileName.java", fileNavigator.getFileName());
+    assertEquals(79, fileNavigator.getLine());
+    assertEquals(0, fileNavigator.getColumn());
+
+    handler.handleMessage("FileName.java");
+    assertEquals("FileName.java", fileNavigator.getFileName());
+    assertEquals(0, fileNavigator.getLine());
+    assertEquals(0, fileNavigator.getColumn());
+
+    handler.handleMessage("FileName.java:error");
+    assertEquals("FileName.java:error", fileNavigator.getFileName());
+    assertEquals(0, fileNavigator.getLine());
+    assertEquals(0, fileNavigator.getColumn());
+  }
+
+  @Test
+  public void handlerShouldExtractFileNameFromFullWindowsPath() {
+    handler.handleMessage("c:\\FileName.java");
+    assertEquals("c:\\FileName.java", fileNavigator.getFileName());
+    assertEquals(0, fileNavigator.getLine());
+    assertEquals(0, fileNavigator.getColumn());
+
+    handler.handleMessage("c:\\FileName.java:80");
+    assertEquals("c:\\FileName.java", fileNavigator.getFileName());
+    assertEquals(79, fileNavigator.getLine());
+    assertEquals(0, fileNavigator.getColumn());
+
+    handler.handleMessage("c:\\FileName.java:80:20");
+    assertEquals("c:\\FileName.java", fileNavigator.getFileName());
+    assertEquals(79, fileNavigator.getLine());
+    assertEquals(19, fileNavigator.getColumn());
+  }
+
+  @Test
+  public void handlerShouldExtractLineNumberAfterHashCharacter() {
+    handler.handleMessage("FileName.java#80");
+    assertEquals("FileName.java", fileNavigator.getFileName());
+    assertEquals(79, fileNavigator.getLine());
+    assertEquals(0, fileNavigator.getColumn());
+  }
+
+  @Test
+  public void handlerShouldExtractLineAndColumnNumberAfterColon() {
+    handler.handleMessage("FileName.java#80#20");
+    assertEquals("FileName.java", fileNavigator.getFileName());
+    assertEquals(79, fileNavigator.getLine());
+    assertEquals(19, fileNavigator.getColumn());
+  }
+
+  @Test
+  public void handlerShouldExtractLineAndColumnNumberAfterHashCharacter() {
+    handler.handleMessage("FileName.java:80:20");
+    assertEquals("FileName.java", fileNavigator.getFileName());
+    assertEquals(79, fileNavigator.getLine());
+    assertEquals(19, fileNavigator.getColumn());
+  }
+}
+
+class StubFileNavigator implements FileNavigator {
+
+  private String fileName;
+  private int line;
+  private int column;
+  private int offsetline;
+
+  @Override
+  public void findAndNavigate(String fileName, int line, int column, int offsetline) {
+    this.fileName = fileName;
+    this.line = line;
+    this.column = column;
+    this.offsetline = offsetline;
+  }
+
+  public String getFileName() {
+    return fileName;
+  }
+
+  public int getLine() {
+    return line;
+  }
+
+  public int getColumn() {
+    return column;
+  }
+
+  public int getOffsetline() {
+    return offsetline;
+  }
+
+
+}
---
+++ src/test/java/com/appetizer/intellij/remotecall/notifier/SocketMessageNotifierTest.java
@@ -0,0 +1,114 @@
+package com.appetizer.intellij.remotecall.notifier;
+
+import com.appetizer.intellij.remotecall.handler.MessageHandler;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class SocketMessageNotifierTest {
+
+  private static final int PORT = 62775;
+  private Thread notifierThread;
+  private final StubMessageHandler messageHandler = new StubMessageHandler();
+  private ServerSocket socket;
+
+  @Before
+  public void setUp() throws IOException {
+    MessageNotifier notifier = createNotifier();
+    notifier.addMessageHandler(messageHandler);
+
+    notifierThread = new Thread(notifier);
+    notifierThread.start();
+  }
+
+  @After
+  public void tearDown() throws IOException {
+    messageHandler.clear();
+    notifierThread.interrupt();
+    disposeNotifier();
+  }
+
+  @Test
+  public void notifierShouldCallHandlerOnMessageReceived() throws IOException {
+    sendMessage("GET /?message=HelloFile.java");
+    assertEquals("HelloFile.java", messageHandler.getLastMessage());
+  }
+
+  @Test
+  public void notifierShouldSkipEmptyMessages() throws IOException {
+    sendMessage("");
+    assertNull("Received " + messageHandler.getLastMessage() + ". Null expected", messageHandler.getLastMessage());
+    messageHandler.clear();
+  }
+
+  @Test
+  public void notifierShouldReceiveOnlyGetRequests() throws IOException {
+    sendMessage("GET /?message=foo");
+    assertEquals("foo", messageHandler.getLastMessage());
+    messageHandler.clear();
+
+    sendMessage("POST /\r\n\r\nmessage=bar");
+    assertNull("Received " + messageHandler.getLastMessage() + ". Null expected", messageHandler.getLastMessage());
+    messageHandler.clear();
+
+    sendMessage("DELETE /?message=bar");
+    assertNull("Received " + messageHandler.getLastMessage() + ". Null expected", messageHandler.getLastMessage());
+    messageHandler.clear();
+  }
+
+  private MessageNotifier createNotifier() throws IOException {
+    socket = new ServerSocket();
+    socket.bind(new InetSocketAddress("localhost", PORT));
+    return new SocketMessageNotifier(socket);
+  }
+
+  private static void sendMessage(String message) throws IOException {
+    Socket client = new Socket("localhost", PORT);
+    try {
+      client.getOutputStream().write(message.getBytes());
+    }
+    finally {
+      client.close();
+    }
+  }
+
+  private void disposeNotifier() throws IOException {
+    socket.close();
+  }
+}
+
+class StubMessageHandler implements MessageHandler {
+
+  private final BlockingQueue<String> messages = new LinkedBlockingQueue<String>();
+
+  @Override
+  public void handleMessage(String message) {
+    messages.add(message);
+  }
+
+  public String getLastMessage() {
+    try {
+      return messages.poll(1, TimeUnit.SECONDS);
+    }
+    catch (InterruptedException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  public void clear() {
+    messages.clear();
+  }
+
+}
+
